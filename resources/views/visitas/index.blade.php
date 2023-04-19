51f0e0cfaac12f1fb4d196bfbc65a08368e479c7
tinwol752 <tinwol752@gmail.com>
2023-04-19 00:04:41 -0500

    Tabla visitas enlasada a la base de datos

--- resources/views/visitas/index.blade.php
+++ resources/views/visitas/index.blade.php
@@ -6,36 +6,31 @@
     </x-slot>
 
     <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <a href="{{route('visita.create')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Registrar</a>
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{-- Modal  --}}
-
-
-                    {{-- Cerrar modal --}}
+                
                     <div class="relative overflow-x-auto">
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead
                                 class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
-                                        Fecha
+                                        Visitante
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Entidad visitada
+                                        DNI 
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Visistante
+                                        Entidad del visitado
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Documento del visitante
+                                        telefono  
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Entidad visitada 
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Funcionario visitado
+                                        Numero de visitantes  
                                     </th>
                                     <th scope="col" class="px-6 py-3">
                                         Hora ingreso
@@ -44,33 +39,66 @@
                                         Hora salida 
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Motivo
+                                        Fecha
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Asunto
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Opciones
                                     </th>
                                 </tr>
+                                
                             </thead>
                             @foreach ($visita as $item)
                                 <tbody>
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                         <th scope="row"
                                             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {{ $item->asunto }}
+                                            {{ $item->visitante->nombre}} {{ $item->visitante->a_paterno}} {{ $item->visitante->a_materno}} 
+
                                         </th>
+
                                         <td class="px-6 py-4">
-                                            {{ $item->visitante->nombre }} {{ $item->visitante->a_paterno }}
-                                            {{ $item->visitante->a_materno }}
+                                            {{ $item->visitante->dni}} 
                                         </td>
                                         <td class="px-6 py-4">
-                                            {{ $item->periodo->fecha }}
+                                            {{ $item->visitante->institucion}}    
                                         </td>
                                         <td class="px-6 py-4">
-                                            Editar/Eliminar
+                                            {{ $item->visitante->telefono }}    
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $item->visitante->num_visitantes}}    
+                                        </td>
+
+                                        <td class="px-6 py-4">
+                                            {{ $item->periodo->hora_inicio}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $item->periodo->hora_fin}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $item->periodo->fecha}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $item->asunto}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</button>
+                                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Eliminar</button>
+                                                
                                         </td>
                                     </tr>
                                 </tbody>
                             @endforeach
+                            
                         </table>
                     </div>
 
+                    <br>
+                        <a href="{{route('visita.create')}}" class="ml-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Registrar</a>
+                    </br>
                     {{-- <ul>@foreach ($visita as $visitas)
                     <li>{{$visitas->asunto}}</li>
                     <li>{{$visitas->visitante->nombre}}</li>
