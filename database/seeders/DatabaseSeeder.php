a2ab4e0324b4d2a360f7150863a3b80e8179ab06
IvanCA <104946207+NaviCA21@users.noreply.github.com>
2023-04-20 10:48:01 -0500

    tipo de usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\tipo_usuario;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,17 +15,33 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $tipoUsuarioAdmin = \App\Models\tipo_usuario::create([
 
-        \App\Models\User::factory()->create([
-            'name' => 'Administrador',
-            'email' => 'administrador@gmail.com',
-            'password' =>  bcrypt('administrador'),
+            'tipo_usuario' => 'Administrador'
         ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'Oscar',
-            'email' => 'oscar@gmail.com',
-            'password' =>  bcrypt('12345678'),
+        $tipoUsuarioRegular = \App\Models\tipo_usuario::create([
+            'tipo_usuario' => 'Usuario'
         ]);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Usuario',
+            'email' => 'usuario@gmail.com',
+            'password' => bcrypt('usuario'),
+            'tipo_usuario_id' => $tipoUsuarioRegular->id
+        ]);
+
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Administrador',
+        //     'email' => 'administrador@gmail.com',
+        //     'password' =>  bcrypt('administrador'),
+        // ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Oscar',
+        //     'email' => 'oscar@gmail.com',
+        //     'password' =>  bcrypt('12345678'),
+        // ]);
     }
 }
